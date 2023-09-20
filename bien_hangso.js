3a8d35a41e7ba910e817c0392fd22a54f9b7d519
Chuku99 <thanhtruc30699@gmail.com>
2023-09-20 19:40:39 +0700

    a

--- bien_hangso.js
+++ bien_hangso.js
@@ -1,3 +1,32 @@
-let x = 10;
-var y = 1.2;
-let z; // z = undefined
+console.log("Hello world")
+
+//Bai 1
+//cau a
+console.log("Hello world")
+//cau b
+console.log("Tran Thanh Truc")
+//cau c
+// console.clear()
+//cau d
+console.log("*****")
+console.log("*****")
+console.log("*****")
+console.log("*****")
+console.log("*****")
+
+//cau e
+console.log("*")
+console.log("***")
+console.log("*****")
+console.log("*******")
+console.log("*********")
+console.log("************")
+
+//cau f
+console.log(" ** ** ")
+console.log("*  *  *")
+console.log("*     *")
+console.log(" *   * ")
+console.log("  * *  ")
+console.log("   *   ")
+
